c3b6851fa83dd0b231bd054d6473725c6898f589
SByndz12 <shraddhabyndoor12@gmail.com>
2020-12-31 20:05:36 +0530

    FINAL

--- labs/admin.py
+++
@@ -1,4 +0,0 @@
-from django.contrib import admin
-from labs.models import Labs
-# Register your models here.
-admin.site.register(Labs)
--- labs/urls.py
+++
@@ -1,4 +0,0 @@
-from . import views
-
-urlpatterns =[path('tests', views.tests, name='tests'),
-            ]
